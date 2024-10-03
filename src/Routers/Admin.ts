d20c2eff9c47c044ef7f45cf2496a4ce3d0227d9
Lumina <erik04.nivala22@gmail.com>
2024-10-03 00:16:56 -0500

    added safety checks to admin API route

--- src/Routers/Admin.ts
+++ src/Routers/Admin.ts
@@ -1,33 +1,39 @@
 import { Router, Request, Response, response } from "express";
 import fs from "fs";
-import { currentLogFile, TLogType } from "../Logging";
+import log, { currentLogFile, TLogType } from "../Logging";
+import { errorMonitor } from "events";
 
 const AdminRoute = Router();
 let lastSentLogEntry: TLogType;
 
 const getNewLogs = (logData: string) => {
-  const newLogs: string[] = [];
-  const logLines = logData.trim().split("\n");
-  if (!lastSentLogEntry) lastSentLogEntry = JSON.parse(logLines[0]);
+  try {
+    const newLogs: string[] = [];
+    const logLines = logData.trim().split("\n");
+    if (!lastSentLogEntry) lastSentLogEntry = JSON.parse(logLines[0]);
 
-  const logJson = logLines.map((line) => JSON.parse(line));
-  const newestLog = logJson[logJson.length - 1];
+    const logJson = logLines.map((line) => JSON.parse(line));
+    const newestLog = logJson[logJson.length - 1];
 
-  if (newestLog !== lastSentLogEntry) {
-    let temp = newestLog;
-    let index = 1;
-    while (
-      temp.time > lastSentLogEntry.time &&
-      logJson.length - 1 > index &&
-      newestLog !== lastSentLogEntry
-    ) {
-      newLogs.push(logLines[logLines.length - index]);
-      index++;
-      temp = logJson[logJson.length - index];
+    if (newestLog !== lastSentLogEntry) {
+      let temp = newestLog;
+      let index = 1;
+      while (
+        temp.time > lastSentLogEntry.time &&
+        logJson.length - 1 > index &&
+        newestLog !== lastSentLogEntry
+      ) {
+        newLogs.push(logLines[logLines.length - index]);
+        index++;
+        temp = logJson[logJson.length - index];
+      }
+      lastSentLogEntry = newestLog;
     }
-    lastSentLogEntry = newestLog;
+    return newLogs;
+  } catch (error) {
+    log.error(error);
+    return undefined;
   }
-  return newLogs;
 };
 
 AdminRoute.get("/api/admin/logs", (request: Request, response: Response) => {
@@ -35,39 +41,43 @@ AdminRoute.get("/api/admin/logs", (request: Request, response: Response) => {
   response.setHeader("Cache-Control", "no-store");
   response.setHeader("Connection", "keep-alive");
 
-  fs.readFile(currentLogFile, "utf-8", (err, data) => {
-    if (err) {
-      console.error("Error reading log file:", err);
-      response.status(500).send("Error reading log file");
-      return;
-    }
-
-    getNewLogs(data)
-      .reverse()
-      .map((log) => {
-        response.write(`data: ${log}\n\n`);
-        response.flush();
-      });
-  });
-
-  fs.watchFile(currentLogFile, (_current, _previous) => {
+  try {
     fs.readFile(currentLogFile, "utf-8", (err, data) => {
       if (err) {
         console.error("Error reading log file:", err);
+        response.status(500).send("Error reading log file");
         return;
       }
+
       getNewLogs(data)
-        .reverse()
+        ?.reverse()
         .map((log) => {
           response.write(`data: ${log}\n\n`);
           response.flush();
         });
     });
-  });
 
-  request.on("close", () => {
+    fs.watchFile(currentLogFile, (_current, _previous) => {
+      fs.readFile(currentLogFile, "utf-8", (err, data) => {
+        if (err) {
+          console.error("Error reading log file:", err);
+          return;
+        }
+        getNewLogs(data)
+          ?.reverse()
+          .map((log) => {
+            response.write(`data: ${log}\n\n`);
+            response.flush();
+          });
+      });
+    });
+
+    request.on("close", () => {
+      response.end();
+    });
+  } catch (error) {
     response.end();
-  });
+  }
 });
 
 export { AdminRoute };
